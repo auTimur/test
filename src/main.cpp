e23a89a410e3923a2212b0bdd1dc75477a2ba46d
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-12-24 11:41:17 +0600

    dobavil

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,12 @@ int myFunction(int, int);
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  pinMode(13,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(13,1);
 }
 
 // put function definitions here:
