fc81481749ae715e36f3215367a88c7c474e31fc
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-12-24 18:54:24 +0600

    Blink2

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
 void setup() {
   pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
 }
 void loop() {
   digitalWrite(13,1);
   delay(10000);
   digitalWrite(13,0);
+  delay(10000);
+  digitalWrite(12,1);
+  delay(10000);
+  digitalWrite(12,0);
   delay(10000);
 }
