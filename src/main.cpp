774284c4aaa55768415a9952040e22fbff016770
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-12-24 13:36:19 +0600

    Blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,10 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
   pinMode(13,OUTPUT);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
   digitalWrite(13,1);
   delay(10000);
   digitalWrite(13,0);
   delay(10000);
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
